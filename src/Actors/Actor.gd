b033c4ac918e3af735df7db850dea00c4016bdf4
Samuel Kenji <samuelkenji@Samuels-MacBook-Pro.local>
2023-03-28 14:58:09 -0300

    setting up

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+class_name Actor
+
+@export var speed = 200
+@export var friction = 0.01
+@export var acceleration = 0.5
+
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		input.x += 1
+	if Input.is_action_pressed('ui_left'):
+		input.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		input.y += 1
+	if Input.is_action_pressed('ui_up'):
+		input.y -= 1
+	return input
+
+func _physics_process(delta):
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+	move_and_slide()
